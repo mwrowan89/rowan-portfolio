13871d26189159ca215b0c811d27df5eb14016f8
mwrowan89 <mwrowan89@gmail.com>
2024-05-20 15:29:44 -0600

    Background added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <div className='Bio'> 
+        <img className='Profile-picture' src='Goat.png' alt='profile picture' />
+          <h1>Michael Rowan</h1>
+          <p>The best there ever was</p>
+
+        </div>
+      
       </header>
     </div>
   );
