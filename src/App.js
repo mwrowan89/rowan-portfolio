5875bca5e758ffed819f8dc526873947b9a9786c
mwrowan89 <mwrowan89@gmail.com>
2024-05-20 16:00:20 -0600

    Added favicon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <div className='Bio'> 
-        <img className='Profile-picture' src='Goat.png' alt='profile picture' />
+        <div className="Bio">
+          <img
+            className="Profile-picture"
+            src="Goat.png"
+            alt="profile picture"
+          />
           <h1>Michael Rowan</h1>
-          <p>Hi, my name is Mike and I have a diverse background that has equipped me with important skills applicable in many areas. As an Eagle scout, I developed strong teamwork skills through numerous mission trips. Guiding teams of 10 - 30 volunteers, building community projects, outreach programs and educational assemblies helped me develop strong organizational and management skills. Additionally, my experience as a paramedic taught me how to handle emergencies and take initiative. Working as an emergency room technician we successfully facilitated the triaging of over 50 patients per hour throughout many 12-hour shifts. 
-
-I further developed my entrepreneurial goals by starting my own woodworking business, where I successfully managed a team of 10 - 15 craftsmen and supplied tables to the largest restaurant group in Chicago, delivering 100% of work on time or early and within budget. Throughout my journey in carpentry, I always sought out the latest advancements in technology, working closely with computer programs like AutoCAD to enhance precision to less than a 1% margin of error leading to increased client satisfaction. This experience with cutting edge technologies sparked my interest in the growing field of software engineering. I joined a software engineering boot camp focused on 10+ languages to further develop my skills and I haven't looked back since. Now, I'm excited to find a company that aligns with my values of teamwork and a passion for learning, where I can continue to grow in the world of software engineering.
-
-Proud to be an innovative and entrepreneurial-minded professional with a proven ability to adapt to new challenges and deliver results in fast-paced environments. Passionate about creating efficient and user-friendly applications that meet the evolving needs of the users and companies alike. Engaged in finding an entry-level position that will support my continuing growth as a software developer.</p>
-
+          <p>
+            I am a versatile professional with a diverse background in teamwork,
+            emergency response, management, and entrepreneurship. As an Eagle
+            Scout and paramedic, I honed my skills in leadership and emergency
+            handling. I later founded a successful woodworking business,
+            utilizing technology like AutoCAD to achieve high precision and
+            client satisfaction. My passion for technology led me to a software
+            engineering boot camp, where I mastered multiple programming
+            languages. I'm now seeking an entry-level software developer
+            position to apply my skills in creating efficient and user-friendly
+            applications. Click the links below to learn more or get in touch.
+          </p>
+          <div className="Contacts">
+            <p>LinkedIn || Gmail || GitHub || MySpace</p>
+            
+          </div>
         </div>
-      
       </header>
     </div>
   );
