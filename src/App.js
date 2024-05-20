6713435ea7722b35291bc4246927b2fdc107663e
mwrowan89 <mwrowan89@gmail.com>
2024-05-20 15:41:19 -0600

    Fixed background issue

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
         <div className='Bio'> 
         <img className='Profile-picture' src='Goat.png' alt='profile picture' />
           <h1>Michael Rowan</h1>
-          <p>The best there ever was</p>
+          <p>Hi, my name is Mike and I have a diverse background that has equipped me with important skills applicable in many areas. As an Eagle scout, I developed strong teamwork skills through numerous mission trips. Guiding teams of 10 - 30 volunteers, building community projects, outreach programs and educational assemblies helped me develop strong organizational and management skills. Additionally, my experience as a paramedic taught me how to handle emergencies and take initiative. Working as an emergency room technician we successfully facilitated the triaging of over 50 patients per hour throughout many 12-hour shifts. 
+
+I further developed my entrepreneurial goals by starting my own woodworking business, where I successfully managed a team of 10 - 15 craftsmen and supplied tables to the largest restaurant group in Chicago, delivering 100% of work on time or early and within budget. Throughout my journey in carpentry, I always sought out the latest advancements in technology, working closely with computer programs like AutoCAD to enhance precision to less than a 1% margin of error leading to increased client satisfaction. This experience with cutting edge technologies sparked my interest in the growing field of software engineering. I joined a software engineering boot camp focused on 10+ languages to further develop my skills and I haven't looked back since. Now, I'm excited to find a company that aligns with my values of teamwork and a passion for learning, where I can continue to grow in the world of software engineering.
+
+Proud to be an innovative and entrepreneurial-minded professional with a proven ability to adapt to new challenges and deliver results in fast-paced environments. Passionate about creating efficient and user-friendly applications that meet the evolving needs of the users and companies alike. Engaged in finding an entry-level position that will support my continuing growth as a software developer.</p>
 
         </div>
       
