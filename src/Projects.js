417506144f45df68cf28a44589ce905e78ac492d
mwrowan89 <mwrowan89@gmail.com>
2024-05-24 12:18:08 -0600

    Added brewery finder

--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import "./Projects.css"
+import React from "react";
+import "./Projects.css";
 
 function Projects() {
   return (
-
-    <div className='Header'>
-        Projects
+    <div className="Projects">
+      <div className="Header">Projects</div>
+      <div>
+        <h1>Brewery Finder</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
